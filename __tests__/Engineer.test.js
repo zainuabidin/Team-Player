adbf110030775e82e6e5b243bac08e64dfc2dfec
Zain Abidin <xan.abdn@gmail.com>
2022-03-06 19:54:56 +0000

    test pass for engineer constructor

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,9 @@
 const Engineer = require('../lib/Engineer');
-const engineer = new Engineer('zain', '123456', 'xan.abdn@gmail.com', 'zainuabidin');
+const engineer = new Engineer('andrea', '123456', 'zain.111@hotmail.co.uk', 'zainuabidin');
+
+test('to confirm if we can retrieve engineer object values ', () => {
+    expect(engineer.name).toBe('andrea');
+    expect(engineer.id).toBe('123456');
+    expect(engineer.email).toBe('zain.111@hotmail.co.uk');
+    expect(engineer.github).toBe('zainuabidin');
+});
