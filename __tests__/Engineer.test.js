fc91942d8a962532670eecb53cb10afe8bd73e64
Zain Abidin <xan.abdn@gmail.com>
2022-03-06 19:59:01 +0000

    all engineer test are passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -7,3 +7,23 @@ test('to confirm if we can retrieve engineer object values ', () => {
     expect(engineer.email).toBe('zain.111@hotmail.co.uk');
     expect(engineer.github).toBe('zainuabidin');
 });
+
+test('test if we can get the name from the getName() method', () => {
+    expect(engineer.getName()).toBe('andrea');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    expect(engineer.getId()).toBe('123456');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+    expect(engineer.getEmail()).toBe('zain.111@hotmail.co.uk');
+});
+
+test('test if we can get the github username from the getGithub() method', () => {
+    expect(engineer.getGithub()).toBe('zainuabidin');
+});
+
+test('test if we can get the role from the getRole() method', () => {
+    expect(engineer.getRole()).toBe('Engineer');
+});
