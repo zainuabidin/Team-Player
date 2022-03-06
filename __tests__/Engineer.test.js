a5bde91db5d99198c54c8402bd91064f6f1c8340
Zain Abidin <xan.abdn@gmail.com>
2022-03-06 21:30:57 +0000

    all the employee test passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,15 +1,15 @@
 const Engineer = require('../lib/Engineer');
-const engineer = new Engineer('andrea', '123456', 'zain.111@hotmail.co.uk', 'zainuabidin');
+const engineer = new Engineer('zain', '123456', 'zain.111@hotmail.co.uk', 'zainuabidin');
 
 test('to confirm if we can retrieve engineer object values ', () => {
-    expect(engineer.name).toBe('andrea');
+    expect(engineer.name).toBe('zain');
     expect(engineer.id).toBe('123456');
     expect(engineer.email).toBe('zain.111@hotmail.co.uk');
     expect(engineer.github).toBe('zainuabidin');
 });
 
 test('test if we can get the name from the getName() method', () => {
-    expect(engineer.getName()).toBe('andrea');
+    expect(engineer.getName()).toBe('zain');
 });
 
 test('test if we can get the id from the getId() method', () => {
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,29 @@
-// write test
+const Intern = require('../lib/Intern');
+const intern = new Intern('zain', '123456', 'zain.111@hotmail.co.uk', 'MatthewBolton');
+
+test('test if we can get the constructor values for the engineer object', () => {
+    expect(intern.name).toBe('zain');
+    expect(intern.id).toBe('123456');
+    expect(intern.email).toBe('zain.111@hotmail.co.uk');
+    expect(intern.school).toBe('MatthewBolton');
+});
+
+test('test if we can get the name from the getName() method', () => {
+    expect(intern.getName()).toBe('zain');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    expect(intern.getId()).toBe('123456');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+    expect(intern.getEmail()).toBe('zain.111@hotmail.co.uk');
+});
+
+test('test if we can get the school name from the getSchool() method', () => {
+    expect(intern.getSchool()).toBe('MatthewBolton');
+});
+
+test('test if we can get the role from the getRole() method', () => {
+    expect(intern.getRole()).toBe('Intern');
+});
